24ed2a4f64a5ed890317716de666d15895a73ba7
Janith Dilshan nawarathne <124287567+gjdnawarathna@users.noreply.github.com>
2024-06-11 19:51:30 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap Page</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+</head>
+<body>
+    <!-- Navigation -->
+    <?php include 'Navigation/Navigation.php'; ?>
+    
+
+    <!-- Content -->
+    <div class="container">
+        <h1>Welcome to ග්‍රීeen Hostel . NSBM !</h1>
+        
+    </div>
+    <div class="slider-item" style="background-image:url(images/home2.jpg);">
+      	<div class="overlay"></div>
+        <div class="container">
+          <div class="row no-gutters slider-text align-items-center justify-content-center">
+          <div class="col-md-12 ftco-animate text-center">
+          	<div class="text mb-5 pb-3">
+	            <h1 class="mb-3">Enjoy the beauty of the rural environment</h1>
+	            <h2>Join With Us</h2>
+            </div>
+            <section class="ftco-section ftc-no-pb ftc-no-pt">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-5 p-md-5 img img-2 d-flex justify-content-center align-items-center" style="background-image: url(images/about\ bording\ image.jpg);">
+					</div>
+					<div class="col-md-7 py-5 wrap-about pb-md-5 ftco-animate">
+	          <div class="heading-section heading-section-wo-line pt-md-5 pl-md-5 mb-5">
+	          	<div class="ml-md-0">
+		          	<span class="subheading">Welcome to ග්‍රීeen Hostel.NSBM</span>
+		            <h2 class="mb-4">Welcome To Our Website</h2>
+	            </div>
+	          </div>
+	          <div class="pb-md-5">
+				<p>"Welcome to your gateway to extraordinary living experiences. Explore our curated collection of rental accommodations tailored to your lifestyle. From cozy retreats to urban sanctuaries, discover the perfect place to call home. Start your journey with us and unlock a world of comfort, convenience, and endless possibilities."
+					<p>This page will list some of the accommodations near the NSBM Green University in Sri Lanka. There are various types of properties starting from apartments, houses, flats available for Rent in Sri Lanka. On our website, you will find many different types of properties such as Residential Properties in Sri Lanka for Rent, Commercial Properties in Sri Lanka for Rent and Lands for Rent in Sri Lanka. These types of properties are in high demand around Homagama and there are a lot of tourists flowing into Sri Lanka.</p>
+					<ul class="ftco-social d-flex">
+                <li class="ftco-animate"><a href="https://twitter.com/nsbm_srilanka"><span class="icon-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="https://web.facebook.com/nsbm.lk/?_rdc=1&_rdr"><span class="icon-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="https://www.nsbm.ac.lk/"><span class="icon-google"></span></a></li>
+                </ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+        <footer class="ftco-footer ftco-bg-dark ftco-section">
+		<div class="container">
+		  <div class="row mb-5">
+			<div class="col-md">
+			  <div class="ftco-footer-widget mb-4">
+				<h2 class="ftco-heading-2">ග්‍රීeen Hostel . NSBM</h2>
+				<p>Accommodations near NSBM Green University include apartments, houses, and flats for rent, catering to diverse needs in Homagama, Sri Lanka.</p>
+				<ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
+				  <li class="ftco-animate"><a href="https://twitter.com/nsbm_srilanka"><span class="icon-twitter"></span></a></li>
+				  <li class="ftco-animate"><a href="https://web.facebook.com/nsbm.lk/?_rdc=1&_rdr"><span class="icon-facebook"></span></a></li>
+				  <li class="ftco-animate"><a href="https://www.nsbm.ac.lk/"><span class="icon-google"></span></a></li>
+				</ul>
+			  </div>
+			</div>
+			<div class="col-md">
+			  <div class="ftco-footer-widget mb-4 ml-md-5">
+				<h2 class="ftco-heading-2">Menu Links</h2>
+				<ul class="list-unstyled">
+				  <li><a href="index.html" class="py-2 d-block">Home</a></li>
+				  <li><a href="rooms.html" class="py-2 d-block">Rooms</a></li>
+				  <li><a href="about.html" class="py-2 d-block">About</a></li>
+				  <li><a href="map.html" class="py-2 d-block">Map</a></li>
+				  <li><a href="contact.html" class="py-2 d-block">Contact</a></li>
+				  </ul>
+			  </div>
+			</div>
+			<div class="col-md">
+			  <div class="ftco-footer-widget mb-4">
+				  <h2 class="ftco-heading-2">Contact Us</h2>
+				  <div class="block-23 mb-3">
+					<ul>
+					  <li><span class="icon icon-map-marker"></span><span class="text">NSBM Green University Town, Mahenwatte, Pitipana, Homagama, 10206 Colombo</span></li>
+					  <li><a href="#"><span class="icon icon-phone"></span><span class="text">+94 115 445 000</span></a></li>
+					  <li><span class="icon icon-envelope"></span><a href="https://www.nsbm.ac.lk">www.nsbm.ac.lk </a></span></a></li>
+					</ul>
+				  </div>
+			  </div>
+			</div>
+		  </div>
+		  <div class="row">
+			<div class="col-md-12 text-center">
+		</div>
+		  </div>
+		</div>
+	  </footer>
+      <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+    <!-- Bootstrap JS and jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+</body>
+</html>
